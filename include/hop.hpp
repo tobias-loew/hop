4bdada16055c8ec366088300eb79031a96d35886
LoewT <Tobias.Loew@steag.com>
2021-05-03 23:59:15 +0200

    added tuple_for_each

--- include/hop.hpp
+++ include/hop.hpp
@@ -1730,6 +1730,22 @@ namespace hop {
     }
 
 
+    // applying each element of tuple to function
+
+    namespace impl {
+        template <class Tuple, class F, std::size_t... I>
+        constexpr F tuple_for_each_helper(Tuple&& t, F&& f, std::index_sequence<I...>)
+        {
+            return (void)std::initializer_list<int>{(std::forward<F>(f)(std::get<I>(std::forward<Tuple>(t))), 0)...}, f;
+        }
+    }
+    template <class Tuple, class F>
+    constexpr F tuple_for_each(Tuple&& t, F&& f)
+    {
+        return impl::tuple_for_each_helper(std::forward<Tuple>(t), std::forward<F>(f),
+            std::make_index_sequence<std::tuple_size<std::remove_reference_t<Tuple>>::value>{});
+    }
+
 
 
     template<class _Overload, size_t index>
